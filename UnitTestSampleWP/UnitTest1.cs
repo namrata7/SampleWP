8f0be95a77b4a7a49bf0d61fbc7576a9e6fe75b4
namrata7 <patilnamratam@yahoo.co.uk>
2015-06-04 17:00:34 +0100

    WordsWithCount method added and test project

---
+++ UnitTestSampleWP/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SampleWP;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSampleWP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ResultNotEmpty()
+        {
+            WordProcessor wordProcessor = new WordProcessor();
+            List<string> list = new List<string>() { "aaa" };
+            var result = wordProcessor.WordsWithCount(list);
+            Assert.IsNotNull(result, "Result Empty");
+        }
+
+        [TestMethod]
+        public void SingleStringReturns1()
+        {
+            WordProcessor wordProcessor = new WordProcessor();
+            List<string> list = new List<string>() { "aaa" };
+            var result = wordProcessor.WordsWithCount(list);
+            foreach (var word in result)
+            {
+                Assert.AreEqual<int>(1, word.Count(), "Single String does not returns count 1");
+            }
+        }
+    }
+}
